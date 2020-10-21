a8c41b85f1f2e3941d179f7ddfd87e73adfa0dc6
AdamHowlett1 <47778935+AdamHowlett1@users.noreply.github.com>
2020-10-21 18:23:41 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@
                     {
                         scrollTop: target.offset().top,
                     },
-                    1000,
+                    100,
                     "easeInOutExpo"
                 );
                 return false;
